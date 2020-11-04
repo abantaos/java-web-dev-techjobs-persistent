fca55da96b7f64506fb99e59c3cbbb4ebf34da76
Sarah <abantaos@hotmail.com>
2020-11-04 15:50:33 -0600

    added data layers

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
 
+    @NotNull(message="Location is required")
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String extraDescription;
+
+    public Skill() {}
+
+    public String getExtraDescription() {
+        return extraDescription;
+    }
+
+    public void setExtraDescription(String extraDescription) {
+        this.extraDescription = extraDescription;
+    }
 }
